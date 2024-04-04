eacc4ba161aa7f5e9daee2faf387e95801709aaa
FiniteOwl114548 <166026171+FiniteOwl114548@users.noreply.github.com>
2024-04-04 16:26:05 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+player.onChat("chicken", function () {
+    for (let index = 0; index < 1200; index++) {
+        for (let index = 0; index < 100; index++) {
+            mobs.spawn(CHICKEN, pos(0, 10, 0))
+        }
+    }
+})
